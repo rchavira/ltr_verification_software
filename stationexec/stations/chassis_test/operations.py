d25d73887f6e3ee81618d10a13c0184151d26ba1
Rick Chavira <rick.chavira@gmail.com>
2022-02-19 11:22:27 -0800

    Update operations.py

--- stationexec/stations/chassis_test/operations.py
+++ stationexec/stations/chassis_test/operations.py
@@ -67,16 +67,17 @@ class SystemTest(Operation):
 class SensorsTest(Operation):
     def operation_action(self):
         result = True
-        for s in self.sensor_group:
+        # TODO: add other sensors and range groups to operations.json and here.
+        for s in self.tlist:
             v = self.chassis.sensors[s]
-            if v < self.lower_limit or v > self.upper_limit:
+            if v < self.trange[0] or v> self.trange[1]:
                 result = False
             self.save_result(s, v)
 
-        self.save_results("test_result", result)
+        self.save_result("test_result", result)
 
 
-@require_tools("chassis", "gpio")
+""" @require_tools("chassis", "gpio")
 class LeakPortTest(Operation):
     def operation_action(self):
         self.gpio.set(self.gpio_out, 0)
@@ -89,7 +90,7 @@ class LeakPortTest(Operation):
         self.gpio.set(self.gpio_out, 1)
         self.chassis.read_inputs()
         self.save_result("s3_leak_detected", s3)
-        self.save_result("leak_report", rpt)
+        self.save_result("leak_report", rpt) """
 
 
 @require_tools("chassis")
@@ -97,10 +98,27 @@ class PowerTest(Operation):
     def operation_action(self):
         self.chassis_1.set_power(self.power_target)
         self.chassis_1.run_ttv(1)
-        time.sleep(3)
+        time.sleep(1)
         s1 = self.chassis.system_monitoring["s1_running_ttv"]
+
+        start = time.time()
+        while time.time() - start < self.run_time:
+            # check temperature
+            tavg = 0
+            for s in self.tlist:
+                tavg += self.chassis.sensors[s]
+
+            tavg = tavg / 4
+
+            if tavg > self.tmax:
+                self.ui_log("Max Temperature exceeded, stopping test")
+                break
+
+        self.chassis_1.set_power(0)
         self.chassis_1.run_ttv(0)
-        self.save_result("s1_running_ttv", s1)
+
+        self.save_result("temp", s1)
+
 
 
 @require_tools("remote", "ip_tool")
